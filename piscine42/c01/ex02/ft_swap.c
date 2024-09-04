08044402ba801dd655256216ce9ef640ceba886f
Tolga Uzan <70696615+tolgauzan@users.noreply.github.com>
2024-09-04 23:06:50 +0300

    Update ft_swap.c

--- piscine42/c01/ex02/ft_swap.c
+++ piscine42/c01/ex02/ft_swap.c
@@ -18,14 +18,5 @@ void	ft_swap(int *a, int *b)
 	*a = *b;
 	*b = temp;
 }
-int main()
-{
-	int a= 5;
-	int b= 6;
 
- 		ft_swap(&a,&b);
-		printf("a nın yeni degeri %d",a);
-		printf("b nin yeni degeri %d",b);
 
-		return 0;
-} 
