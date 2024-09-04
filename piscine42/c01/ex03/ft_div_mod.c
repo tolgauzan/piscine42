ac3021fe6a17ef9764c4dc16700886f0bbf038fb
Tolga Uzan <70696615+tolgauzan@users.noreply.github.com>
2024-09-04 23:09:17 +0300

    Update ft_div_mod.c

--- piscine42/c01/ex03/ft_div_mod.c
+++ piscine42/c01/ex03/ft_div_mod.c
@@ -9,7 +9,7 @@
 /*   Updated: 2024/08/19 14:53:53 by tuzan            ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
+
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
 	if (b != 0)
@@ -18,18 +18,4 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 		*mod = a % b;
 	}
 }
-int main()
-{
-	int num1= 10;
-	int num2= 3;
-	
-	int mod;
-	int *p1 = &mod;
-	int div;
-	int *p2 = &div;
-	ft_div_mod(num1,num2,p1,p2);
 
-	printf(" p1 deger %d",mod);
-	printf(" p2 deger %d",div);
-	return 0;
-}
