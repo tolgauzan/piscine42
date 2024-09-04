82c4b2e1de39fb97cb3dec37427954cec8bdb6b7
Tolga Uzan <70696615+tolgauzan@users.noreply.github.com>
2024-09-04 17:02:30 +0300

    Add files via upload

---
+++ piscine42/c08/ex05/ft_show_tab.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tuzan <tuzan@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 16:31:47 by tuzan             #+#    #+#             */
+/*   Updated: 2024/09/04 16:36:13 by tuzan            ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+void	ft_show_tab(struct s_stock_str *parr)
+{
+	int	i;
+
+	i = 0;
+	while (parr[i].str != 0)
+	{
+		ft_putstr(parr[i].str);
+		ft_putstr("\n");
+		ft_putnbr(parr[i].size);
+		ft_putstr("\n");
+		ft_putstr(parr[i].copy);
+		ft_putstr("\n");
+		i++;
+	}
+}
