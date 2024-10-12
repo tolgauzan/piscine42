0793a600c1455bdeb6c099a498041815271fa639
Tolga Uzan <70696615+tolgauzan@users.noreply.github.com>
2024-10-12 17:55:17 +0000

    Add files via upload

--- libft/libft.h
+++ libft/libft.h
@@ -22,5 +22,8 @@ int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	strlcpy(char *dest, const char *src, size_t size);
 
 #endif
